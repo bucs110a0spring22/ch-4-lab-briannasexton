964401725dbbac00a0d55a4fc65813746100717d
Brianna O <98538246+briannasexton@users.noreply.github.com>
2022-03-08 01:11:03 +0000

    finish lab

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ This program draws then functions sine, cosine, and tangent on a drawn axis.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ N/A
 
 ## REFERENCES:
- < List any outside resources used >
+https://stackoverflow.com/questions/20918749/how-to-draw-sine-in-python-turtle)
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ N/A
--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 import turtle
-
+import math
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
+leo = turtle.Turtle()
+
+def drawSineCurve(leo):
+  for angle in range(-360,360):
+    i=math.sin(math.radians(angle))
+    leo.goto(angle,i)
+    leo.pendown()
+  leo.penup()
+
+def setupWindow(window):
+  window.bgcolor("pink")
+  window.setworldcoordinates(-360,-1,360,1)
+  leo.speed(3)
+def setupAxis(leo):
+  leo.goto(-360,0)
+  leo.goto(360,0)
+  leo.goto(0,0)
+  leo.goto(0,1)
+  leo.goto(0,-1)
+  leo.penup()
 
 
+def drawCosineCurve(leo):
+  leo.penup()
+  for angle in range(-360,360):
+    i=math.cos(math.radians(angle))
+    leo.goto(angle,i)
+    leo.pendown()
+  leo.penup()
 
-
-
-
-
+def drawTangentCurve(leo):
+  leo.penup()
+  for angle in range(-360,360):
+    i=math.tan(math.radians(angle))
+    leo.goto(angle,i)
+    leo.pendown()
+  leo.penup()
 
 ##########  Do Not Alter Any Code Past Here ########
 def main():
